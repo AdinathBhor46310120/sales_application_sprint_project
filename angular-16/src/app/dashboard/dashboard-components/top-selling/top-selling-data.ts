42bf59da785562b654e08b1a6533b1fa8a412c70
Suyash Jadhav <suyash.a.jadhav@capgemini.com>
2023-07-05 18:51:59 +0530

    fornt end updates

--- angular-16/src/app/dashboard/dashboard-components/top-selling/top-selling-data.ts
+++ angular-16/src/app/dashboard/dashboard-components/top-selling/top-selling-data.ts
@@ -10,41 +10,43 @@ export interface Product {
 
 export const TopSelling: Product[] = [
 
-    {
-        image: 'assets/images/users/user1.jpg',
-        uname: 'Chai',
-        gmail: '',
-        productName: 'All Seasons',
-        status: 'danger',
-        weeks: 35,
-        budget: '95K'
-    },
-    {
-        image: 'assets/images/users/user2.jpg',
-        uname: 'Tofu',
-        gmail: '',
-        productName: 'Seasonal',
-        status: 'info',
-        weeks: 35,
-        budget: '80K'
-    },
-    {
-        image: 'assets/images/users/user3.jpg',
-        uname: 'Chocolade',
-        gmail: '',
-        productName: 'All easons	',
-        status: 'warning',
-        weeks: 35,
-        budget: '95K'
-    },
-    {
-        image: 'assets/images/users/user4.jpg',
-        uname: 'Ipoh Coffee',
-        gmail: '',
-        productName: 'Seasonal',
-        status: 'success',
-        weeks: 35,
-        budget: '85K'
-    },
+    // {
+    //     image: 'assets/images/users/user1.jpg',
+    //     uname: 'Chai',
+    //     gmail: '',
+    //     productName: 'All Seasons',
+    //     status: 'danger',
+    //     weeks: 35,
+    //     budget: '95K'
+    // },
+    // {
+    //     image: 'assets/images/users/user2.jpg',
+    //     uname: 'Tofu',
+    //     gmail: '',
+    //     productName: 'Seasonal',
+    //     status: 'info',
+    //     weeks: 35,
+    //     budget: '80K'
+    // },
+    // {
+    //     image: 'assets/images/users/user3.jpg',
+    //     uname: 'Chocolade',
+    //     gmail: '',
+    //     productName: 'All easons	',
+    //     status: 'warning',
+    //     weeks: 35,
+    //     budget: '95K'
+    // },
+    // {
+    //     image: 'assets/images/users/user4.jpg',
+    //     uname: 'Ipoh Coffee',
+    //     gmail: '',
+    //     productName: 'Seasonal',
+    //     status: 'success',
+    //     weeks: 35,
+    //     budget: '85K'
+    // },
 
+
+    
 ]
